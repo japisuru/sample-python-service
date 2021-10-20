39d808a20a0ee0d02d53308c021f950e7865b9a2
isuru <jayasooriyaisuru@gmail.com>
2021-10-20 15:23:06 +0530

    Change to post.

--- app/main.py
+++ app/main.py
@@ -1,15 +1,26 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
+
+import uvicorn
 
 app = FastAPI()
 
+class SampleRequest(BaseModel):
+    a: int
+    b: int
 
-@app.get("/add")
-def add(a: int, b: int):
-    return {"result": (a + b)}
+
+@app.post("/add")
+def add(this_request: SampleRequest):
+    return {"result": (this_request.a + this_request.b)}
 
 
-@app.get("/mul")
-def mul(a: int, b: int):
-    return {"result": (a * b)}
+@app.post("/mul")
+def mul(this_request: SampleRequest):
+    return {"result": (this_request.a * this_request.b)}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
