d146ec84e30d0449c7ef8377e3df8fdb5b4fb2f1
isuru <jayasooriyaisuru@gmail.com>
2022-03-16 19:49:50 +0530

    Add temporary saving results to /opt/act/dc/

--- app/main.py
+++ app/main.py
@@ -12,11 +12,38 @@ class SampleRequest(BaseModel):
     b: int
 
 
+dir_path = '/opt/act/dc/'
+
+
 @app.post("/add")
 def add(this_request: SampleRequest):
+    import os
+
+    filename = dir_path + 'results.txt'
+
+    if os.path.exists(filename):
+        append_write = 'a'  # append if already exists
+    else:
+        append_write = 'w'  # make a new file if not
+
+    f = open(filename, append_write)
+    f.write("add : " + str(this_request.a + this_request.b) + '\n')
+    f.close()
     return {"result": (this_request.a + this_request.b)}
 
 
 @app.post("/mul")
 def mul(this_request: SampleRequest):
+    import os
+
+    filename = dir_path + 'results.txt'
+
+    if os.path.exists(filename):
+        append_write = 'a'  # append if already exists
+    else:
+        append_write = 'w'  # make a new file if not
+
+    f = open(filename, append_write)
+    f.write("mul : " + str(this_request.a * this_request.b) + '\n')
+    f.close()
     return {"result": (this_request.a * this_request.b)}
